04330de1620b3fb697543559c47dd58f0e1eb734
vladislavvasyukov <v.vasiukov@dev.kokoc.com>
2020-02-05 20:28:26 +0300

    add two folders

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from sanic import Sanic
+from sanic.response import json, file
+
+app = Sanic()
+
+app.static('/', '../frontend')
+
+@app.route("/")
+async def index(request):
+    return await file('frontend/index.html')
---
+++ run.py
@@ -0,0 +1,2 @@
+from backend import main
+main.app.run(host="0.0.0.0", port=5000)
